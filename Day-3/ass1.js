850e272af40b357cb3430c7042bd4a357e8c2481
rahilshah99 <62378939+rahilshah99@users.noreply.github.com>
2020-07-16 23:07:40 +0530

    Day 3 revised

---
+++ Day-3/ass1.js
@@ -0,0 +1,12 @@
+var num = prompt("Enter the number");
+
+function findOddEven(){
+
+
+    if ( num % 2 == 0) {
+        console.log(num,"is even");
+    }else{
+        console.log(num,"is odd");
+    }
+} 
+findOddEven();       
